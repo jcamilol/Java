98003b929609b6e959feb354659814cb0b467b5a
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-12 14:15:55 -0500

    Código que contiene comandos para salto de línea, agragar comentarios y unir diferentes cadenas de texto al imprimir

---
+++ new_line.java
@@ -0,0 +1,9 @@
+//Este es un comentario de una sola línea
+/*Este es un comentario en
+míltiples líneas*/
+class Hello{
+	public static void main(String[] args) 
+	{
+	System.out.println("Este código "+"simplemente "+"es "+"para\nseparar líneas y unir palabras con +");
+	}
+	   }
