ca06fcd4a478b5f584462204d7e1faf0c77ffe42
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-12 20:39:52 -0500

    Código que imprime distintos tipos de datos

---
+++ data_types.java
@@ -0,0 +1,14 @@
+class D1{
+    public static void main(String[] args){
+	int a = 3, b = 4;
+	float c = 3.1415f, d = 2.1715f;  
+	double e = 13.1414214, f = 11312.14; 
+	char g = 'G', h = 'M';
+	boolean i= true;
+	System.out.println("int: "+a+" "+b);
+	System.out.println("float: "+c+" "+d);
+	System.out.println("double: "+e+" "+f);
+	System.out.println("char: "+g+" "+h);
+	System.out.println("double: "+i);
+    }
+}
