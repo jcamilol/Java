de5a2d5d60222192564b8fc183e33318c9ddb06b
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-15 16:47:00 -0500

    Código que imprime operadores de incremento y decremento

---
+++ increment_decrement_operators.java
@@ -0,0 +1,9 @@
+class code{
+    public static void main(String[] args){
+	int a=10, b=10, c=10, d=10;
+	System.out.println("Value of a++ = "+(a++)); //Postfix ++ operator
+	System.out.println("Value of ++b = "+(++b)); //Prefix ++ operator
+	System.out.println("Value of c-- = "+(c--)); //Postfix -- operator
+	System.out.println("Value of --d = "+(--d)); //Prefix -- operator
+    }
+}
