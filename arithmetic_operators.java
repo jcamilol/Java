783d503c88ba9e95ffeba2d45b334605eee95ccb
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-15 16:42:59 -0500

    Código que incluye operadores aritméticos

---
+++ arithmetic_operators.java
@@ -0,0 +1,11 @@
+class code{
+    public static void main(String[] args){
+	int a=8, b=4;
+	System.out.println("a+b = "+(a+b));
+	System.out.println("a-b = "+(a-b));
+	System.out.println("a*b = "+(a*b));
+	System.out.println("a/b = "+(a/b));
+	System.out.println("a%b = "+(a%b));
+    }
+}
+
