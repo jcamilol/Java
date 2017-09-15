7197439dd4c12390d49d6ed4a19f85df65ae3d3d
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-15 16:45:15 -0500

    Código que incluye operadores lógicos

---
+++ logical_operators.java
@@ -0,0 +1,8 @@
+class code{
+    public static void main (String[] args){
+	int a=10, b=0;
+	System.out.println("!(a>b) = "+!(a>b));
+	System.out.println("(a>b)&&(a==0) = "+((a>b)&&(a==0)));
+	System.out.println("(a>b)||(a==0) = "+((a>b)||(a==0)));
+    }
+}
