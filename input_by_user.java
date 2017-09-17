b597e0ab623fa6072410ab5efbf590aa30791d61
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-16 20:04:36 -0500

    Código que recibe entrada del usuario

---
+++ input_by_user.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class code{
+    public static void main(String[] args){
+        Scanner s1 = new Scanner (System.in);
+        int n;
+        System.out.println("Entre número entero");
+        int a;
+        a = s1.nextInt();
+        System.out.println("El número ingresado es "+a);
+    }
+}
