179404c965d71d2b9e7755f4db7de407e1ae3b3e
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-16 20:07:26 -0500

    Código que recibe una entrada de tipo string (Cadena de palabras) del usuario

---
+++ input_sentences.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class code{
+    public static void main(String[] args){
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Ingrese su nombre completo");
+        String fullname = s1.nextLine();
+        System.out.println("Su nombre completo es "+fullname);
+    }
+}
