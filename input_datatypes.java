b5c18d3369685c03cbe29a2f3d09f1641b174e81
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-16 20:05:32 -0500

    Código que recibe entradas de varios tipos del usuario

---
+++ input_datatypes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class code{
+    public static void main(String[] args){
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Entre número entero");
+        int a;
+        a = s1.nextInt();
+        System.out.println("Entre número double");
+        double b;
+        b = s1.nextDouble();
+        System.out.println("El entero ingresado es "+a);
+        System.out.println("El double ingresado es "+b);
+    }
+}
