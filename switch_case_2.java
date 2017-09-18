be370de329c521049cf46eb6d93337f2a5a7719f
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-17 22:11:41 -0500

    Código de ejemplo del uso de switch...case usando int

---
+++ switch_case_2.java
@@ -0,0 +1,16 @@
+//Este código es un ejemplo del uso del switch...case statement usando enteros
+class code{
+    public static void main(String[] args){
+        int n = 2;
+        switch(n){
+            case 1:
+                System.out.println("El número es 1");
+                break;
+            case 2:
+                System.out.println("El número es 2");
+                break;
+            default:
+                System.out.println("El número es mayor de 2");
+        }
+    }
+}
