7006560b2199f6521184095b908ac6d3a4e8d723
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-17 22:09:26 -0500

    Código de ejemplo del uso de if statement

---
+++ if.java
@@ -0,0 +1,9 @@
+//This is an example of the use of the use of if statements
+class code{
+    public static void main(String[] args){
+        int a=10, b=20;
+        if(a<b){
+            System.out.println(b+" es más grande que "+a);
+        }
+    }
+}
