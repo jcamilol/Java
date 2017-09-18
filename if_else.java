4d0aa42a51e7469e255984ce2bf792dacf3026b8
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-17 22:09:52 -0500

    Código de ejemplo del uso de if...else statement

---
+++ if_else.java
@@ -0,0 +1,12 @@
+//Tis is an example of the use of the use of if... else statements
+class code{
+    public static void main(String[] args){
+        int a=20, b=10;
+        if(a<b){
+            System.out.println(b+" es más grande que "+a);
+        }
+        else{
+            System.out.println(a+" es más grande que "+b);
+        }
+    }
+}
