ed5918440c45aadb145eef1d38abc8c5322f1a03
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-19 21:10:11 -0500

    Código de ejemplo de un ciclo infinito

---
+++ infinite_loop.java
@@ -0,0 +1,8 @@
+//Este código es un ejemplo de ciclo infinito
+class code{
+    public static void main(String[] args){
+        while (0 == 0){
+            System.out.println("Este ciclo nunca acabará");
+        }
+    }
+}
