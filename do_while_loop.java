1cc27c30abb279ade506e874fcdd8f81d199dbb4
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-19 21:09:18 -0500

    Código de ejemplo de un ciclo do...while

---
+++ do_while_loop.java
@@ -0,0 +1,11 @@
+//Este código es un ejemplo del uso del ciclo do...while, con el que se imprimen los números del 1 al 10
+class code{
+    public static void main(String[] args){
+        int ii=1;
+        do{
+            System.out.println(ii);
+            ii++;
+        }
+        while(ii<=10);
+    }
+}
