1a6ef7ff1b0d9af7bbf4aa593f7c9e28d03f7251
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-09-26 16:13:27 -0500

    Código que muestra como calcular el tamaño de un array

---
+++ for_each_loop.java
@@ -0,0 +1,9 @@
+//Este código es un ejemplo del uso del for-each loop
+class code{
+    public static void main(String[] args){
+        int[] ar = {12,3,41,17,92};
+        for(int m : ar){
+            System.out.println(m);
+        }
+    }
+}
---
+++ length_of_array.java
@@ -0,0 +1,7 @@
+//Este código muestra la manera de usar la función predeterminada "array".length para determinar el tamaño de un array
+class code{
+    public static void main(String[] args){
+        int[] z = new int[10];
+        System.out.println(z.length);
+    }
+}
