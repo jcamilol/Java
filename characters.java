0852d62d691f4ef8e21143418aa4381246bfb18d
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-10-02 13:40:52 -0500

    Código de ejemplo del uso de characters en java

---
+++ characters.java
@@ -0,0 +1,9 @@
+//Este código es un ejemplo del uso de characters en java
+class code{
+    public static void main(String[] args){
+        char c1, c2;
+        c1 = 'd';
+        c2 = 88;
+        System.out.println("c1 = "+c1+", c2 = "+c2);
+    }
+}
