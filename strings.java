9f441e4acb29e913d057c784b7ad860c7e33a3ab
JuanCamiloLozano <lozanocamilo2015@gmail.com>
2017-10-02 13:41:19 -0500

    Código de ejemplo del uso de Strings en Java

---
+++ strings.java
@@ -0,0 +1,9 @@
+//Este código es un ejemplo que asigna e imprime el valor de dos strings, uno usando the "new keyword" y el otro sin usarlo
+class code{
+    public static void main(String[] args){
+        String S1 = "Characters and strings";
+        String S2 = new String("Characters and strings");
+        System.out.println(S1);
+        System.out.println(S2);
+    }
+}
